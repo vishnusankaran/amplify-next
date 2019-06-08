3b81285a0e91dadca86cb2fe4f914f96b957fe05
Vishnu Sankaran <INE12363327>
2019-06-09 03:19:24 +0530

    removed component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import ItemList from "../components/item-list";
-
 const Index = () => (
   <React.Fragment>
     <h1>Welcome to Amplify+Next</h1>
     <p>Please create pages in pages folder</p>
-    <ItemList />
   </React.Fragment>
 );
 
